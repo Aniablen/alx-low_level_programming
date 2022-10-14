eba5bf667fb8e1491f6602bddf88a139f80f0572
Aniablen <ptsctan1@gmail.com>
2022-10-14 13:38:46 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print single digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 22; n < 32; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
