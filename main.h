4b3c3b45af2292ff97f19dfc3319f1971a6b03a2
Aniablen <ptsctan1@gmail.com>
2022-10-26 03:53:39 -0700

    task 0

--- main.h
+++ main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_day(int month, int day, int year);
-int convert_day(int month, int day);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+int _putchar(char c);
 
 #endif
